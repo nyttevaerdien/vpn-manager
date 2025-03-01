71973f2a396ab73f34f3a9d5f0c612085690cba6
nyttevaerdien <krfdtv@gmail.com>
2025-03-01 16:10:41 +0100

    added ssl

--- src/main/java/com/kotprotiv/vpn_manager/config/SecurityConfig.java
+++ src/main/java/com/kotprotiv/vpn_manager/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.kotprotiv.vpn_manager.config;
 
+import com.kotprotiv.vpn_manager.model.ServiceUser;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-        return httpSecurity.build();
-    }
+  @Bean
+  @ConfigurationProperties("security.user")
+  public ServiceUser serviceUser() {
+    return new ServiceUser();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .httpBasic(httpSecurityHttpBasicConfigurer -> {})
+        .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(ServiceUser serviceUser) {
+    UserDetails user =
+        User.withDefaultPasswordEncoder()
+            .username(serviceUser.getUsername())
+            .password(serviceUser.getPassword())
+            .roles("USER")
+            .build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
 }
---
+++ src/main/java/com/kotprotiv/vpn_manager/model/ServiceUser.java
@@ -0,0 +1,10 @@
+package com.kotprotiv.vpn_manager.model;
+
+import lombok.Data;
+
+@Data
+public class ServiceUser {
+
+    private String username;
+    private String password;
+}
