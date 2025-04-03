3c191e4e6ec110af96d33e217bcbdcf91bedf7dd
nyttevaerdien <krfdtv@gmail.com>
2025-04-03 11:12:32 +0200

    ip-logging

---
+++ src/main/java/com/kotprotiv/vpn_manager/controller/RequestLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.kotprotiv.vpn_manager.controller;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String ip = request.getRemoteAddr();
+        log.info("Incoming request from IP: {}", ip);
+        chain.doFilter(request, response);
+    }
+
+}
